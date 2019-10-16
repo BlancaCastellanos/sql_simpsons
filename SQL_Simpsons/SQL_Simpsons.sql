9de1c2d5a6b0777f7198f8f452f7d5cc3224c25e
Blanca Lidia Castellanos Solis <53828168+BlancaCastellanos@users.noreply.github.com>
2019-10-16 09:33:38 -0700

    Add files via upload

---
+++ SQL_Simpsons/SQL_Simpsons.sql
@@ -0,0 +1,6 @@
+use db_simpsons;
+
+insert into tbl_character (namee, secondname, lastname, age, descript)
+values ('Margaret', 'Abigail', 'Simpson', '1', 'Hermano de Bart y Lisa');
+
+Select * from tbl_character
